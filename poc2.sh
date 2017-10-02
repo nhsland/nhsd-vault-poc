9ac86249ccf39d8fcdb80b7316bae4fcfb84ff7c
Rob Dyke <robdyke@gmail.com>
2017-10-02 18:57:55 +0100

    Added encrypt/decrypt functions

--- poc2.sh
+++ poc2.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# Script to crypt every cell in a CSV using Vault Transit secret backend
+# Script to encrypt/decrypt every cell in a CSV using Vault Transit secret backend
+
+# Usage:
+# ./poc2.sh filename encrypt|decrypt
 
 # Set URL of Vault instance
 VAULT_ADDR="http://127.0.0.1:8200"
@@ -14,9 +17,12 @@ fi
 # Get path and filename from input arguements
 FILEPATH=`echo $1|cut -d'/' -f1`
 FILENAME=`echo $1|cut -d'/' -f2`
-
 echo $FILENAME
 echo $FILEPATH
+
+# Set ACTION for script
+# Either encrypt or decrypt
+SCRIPTACTION=$2
 
 # Read in the CSV
 while IFS="," read f1 f2 f3 f4 f5 f6 f7
@@ -74,16 +80,40 @@ while IFS="," read f1 f2 f3 f4 f5 f6 f7
       rm patient.json
     }
 
-    # Call functions to encode and crypt each field
-    b64encode $fROW; vaultEncrypt $bVAR; eROW=$vCIPHER;
-    b64encode $fPATIENTID; vaultEncrypt $bVAR; ePATIENTID=$vCIPHER;
-    b64encode $fSMOKER; vaultEncrypt $bVAR; eSMOKER=$vCIPHER;
-    b64encode $fSTROKE; vaultEncrypt $bVAR; eSTROKE=$vCIPHER;
-    b64encode $fLUNGCANCER; vaultEncrypt $bVAR; eLUNGCANCER=$vCIPHER;
-    b64encode $fHEARTDISEASE; vaultEncrypt $bVAR; eHEARTDISEASE=$vCIPHER;
-    b64encode $fRISK; vaultEncrypt $bVAR; eRISK=$vCIPHER;
+    encryptSourceCSV(){
+      # Call functions to encode and crypt each field
+      b64encode $fROW; vaultEncrypt $bVAR; eROW=$vCIPHER;
+      b64encode $fPATIENTID; vaultEncrypt $bVAR; ePATIENTID=$vCIPHER;
+      b64encode $fSMOKER; vaultEncrypt $bVAR; eSMOKER=$vCIPHER;
+      b64encode $fSTROKE; vaultEncrypt $bVAR; eSTROKE=$vCIPHER;
+      b64encode $fLUNGCANCER; vaultEncrypt $bVAR; eLUNGCANCER=$vCIPHER;
+      b64encode $fHEARTDISEASE; vaultEncrypt $bVAR; eHEARTDISEASE=$vCIPHER;
+      b64encode $fRISK; vaultEncrypt $bVAR; eRISK=$vCIPHER;
 
-    # Write out CSV
-    echo "$fROW,$ePATIENTID,$eSMOKER,$eSTROKE,$eLUNGCANCER,$eHEARTDISEASE,$eRISK" >> cipher_$FILENAME.csv
+      # Write out CSV
+      echo "$fROW,$ePATIENTID,$eSMOKER,$eSTROKE,$eLUNGCANCER,$eHEARTDISEASE,$eRISK" >> cipher_$FILENAME
+  }
+
+  decryptSourceCSV(){
+      # Call functions to encode and crypt each field
+      vaultDecrypt $fROW; b64decode $vPLAIN; dROW=$bVAR;
+      vaultDecrypt $fPATIENTID; b64decode $vPLAIN; dPATIENTID=$bVAR;
+      vaultDecrypt $fSMOKER; b64decode $vPLAIN; dSMOKER=$bVAR;
+      vaultDecrypt $fSTROKE; b64decode $vPLAIN; dSTROKE=$bVAR;
+      vaultDecrypt $fLUNGCANCER; b64decode $vPLAIN; dLUNGCANCER=$bVAR;
+      vaultDecrypt $fHEARTDISEASE; b64decode $vPLAIN; dHEARTDISEASE=$bVAR;
+      vaultDecrypt $fRISK; b64decode $vPLAIN; dRISK=$bVAR;
+
+      # Write out CSV
+      echo "$fROW,$dPATIENTID,$dSMOKER,$dSTROKE,$dLUNGCANCER,$dHEARTDISEASE,$dRISK" >> plain_$FILENAME
+    }
+
+    if [ $SCRIPTACTION = encrypt ] ; then
+        encryptSourceCSV;
+    fi
+
+    if [ $SCRIPTACTION = decrypt ] ; then
+        decryptSourceCSV;
+    fi
 
 done < $1
