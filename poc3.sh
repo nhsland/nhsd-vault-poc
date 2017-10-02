7d558ed370b99f51f20b4a083fe7e4f997d3157a
Rob Dyke <robdyke@gmail.com>
2017-10-03 00:20:38 +0100

    Decrypt routine added

--- poc3.sh
+++ poc3.sh
@@ -55,8 +55,6 @@ while IFS="," read f1 f2 f3 f4 f5 f6 f7
 
     # Function to base64 encode input
     b64encode(){
-      # No need to encode the row number
-      # bROW=`echo $f1|base64`
       bPATIENTID=`echo $f2|base64`
       bSMOKER=`echo $f3|base64`
       bSTROKE=`echo $f4|base64`
@@ -67,21 +65,21 @@ while IFS="," read f1 f2 f3 f4 f5 f6 f7
 
     # Function to base64 decode input
     b64decode(){
-      # No need to decode the row number
-      # bROW=`echo $f1|base64 -d`
+      while IFS="," read f1 f2 f3 f4 f5 f6 f7
+      do
       bPATIENTID=`echo $f2|base64 -d`
       bSMOKER=`echo $f3|base64 -d`
       bSTROKE=`echo $f4|base64 -d`
       bLUNGCANCER=`echo $f5|base64 -d`
       bHEARTDISEASE=`echo $f6|base64 -d`
       bRISK=`echo $f7|base64 -d`
+      done < transit_files/decipher_$FILENAME
+      rm transit_files/decipher_$FILENAME
     }
 
     plainVAULTJSON(){
       echo "{" > patient.json
       echo "  \"batch_input\": [" >> patient.json
-      # No need to encrypt the row number
-      # echo "    { \"plaintext\": \"$bROW\" }," >> patient.json
       echo "    { \"plaintext\": \"$bPATIENTID\" }," >> patient.json
       echo "    { \"plaintext\": \"$bSMOKER\" }," >> patient.json
       echo "    { \"plaintext\": \"$bSTROKE\" }," >> patient.json
@@ -92,53 +90,50 @@ while IFS="," read f1 f2 f3 f4 f5 f6 f7
       echo "}" >> patient.json
     }
 
+    cipherVAULTJSON(){
+      echo "{" > patient.json
+      echo "  \"batch_input\": [" >> patient.json
+      echo "    { \"ciphertext\": \"$f2\" }," >> patient.json
+      echo "    { \"ciphertext\": \"$f3\" }," >> patient.json
+      echo "    { \"ciphertext\": \"$f4\" }," >> patient.json
+      echo "    { \"ciphertext\": \"$f5\" }," >> patient.json
+      echo "    { \"ciphertext\": \"$f6\" }," >> patient.json
+      echo "    { \"ciphertext\": \"$f7\" }" >> patient.json
+      echo "  ]" >> patient.json
+      echo "}" >> patient.json
+    }
+
     # Function to encrypt using Vault Transit
     vaultEncrypt(){
       # Send patient.json to Vault
       curl -s --header "X-Vault-Token: $VAULT_TOKEN" --request POST --data @patient.json $VAULT_ADDR/v1/transit/encrypt/foo > patient_encrypt.json
-
       # Remove patient.json now
       rm patient.json
-
       # Process returned json with jq and tr
       encryptROW=`cat patient_encrypt.json | jq --compact-output --raw-output '.data.batch_results | map ([.ciphertext] | join(","))|@text' |tr -d '[' | tr -d ']' | tr -d '"'`
-
       # Remove patient_encrypt.json now
       rm patient_encrypt.json
-
       echo "$f1,$encryptROW" >> transit_files/cipher_$FILENAME
     }
 
-    # Notes on using jq
-    # jq --raw-output '.data.batch_results| .[]|.ciphertext'
-    # jq --compact-output --raw-output '.data.batch_results | map ([.ciphertext] | join(", "))'
-    # jq --compact-output --raw-output '.data.batch_results | map ([.ciphertext] | join(","))|@text'
-
     # Function to encrypt using Vault Transit
-    # vaultDecrypt(){
-    #   echo "{\"ciphertext\":\"$1\"}" > patient.json
-    #   vRESPONSE=`curl -s --header "X-Vault-Token: $VAULT_TOKEN" --request POST --data @patient.json $VAULT_ADDR/v1/transit/decrypt/foo`
-    #   vPLAIN=`echo $vRESPONSE | cut -d '{' -f 3 |cut -d '}' -f 1|cut -d '"' -f4`
-    #   rm patient.json
-    # }
+    vaultDecrypt(){
+      curl -s --header "X-Vault-Token: $VAULT_TOKEN" --request POST --data @patient.json $VAULT_ADDR/v1/transit/decrypt/foo > patient_decrypt.json
+      rm patient.json
+      decryptROW=`cat patient_decrypt.json | jq --compact-output --raw-output '.data.batch_results | map ([.plaintext] | join(","))|@text' |tr -d '[' | tr -d ']' | tr -d '"'`
+      rm patient_decrypt.json
+      echo "$f1,$decryptROW" > transit_files/decipher_$FILENAME
+    }
 
   encryptSourceCSV(){
       b64encode; plainVAULTJSON; vaultEncrypt;
   }
 
   decryptSourceCSV(){
-      # Call functions to encode and crypt each field
-      vaultDecrypt $fROW; b64decode $vPLAIN; dROW=$bVAR;
-      vaultDecrypt $fPATIENTID; b64decode $vPLAIN; dPATIENTID=$bVAR;
-      vaultDecrypt $fSMOKER; b64decode $vPLAIN; dSMOKER=$bVAR;
-      vaultDecrypt $fSTROKE; b64decode $vPLAIN; dSTROKE=$bVAR;
-      vaultDecrypt $fLUNGCANCER; b64decode $vPLAIN; dLUNGCANCER=$bVAR;
-      vaultDecrypt $fHEARTDISEASE; b64decode $vPLAIN; dHEARTDISEASE=$bVAR;
-      vaultDecrypt $fRISK; b64decode $vPLAIN; dRISK=$bVAR;
-
+      cipherVAULTJSON; vaultDecrypt; b64decode;
       # Write out CSV
-      echo "$fROW,$dPATIENTID,$dSMOKER,$dSTROKE,$dLUNGCANCER,$dHEARTDISEASE,$dRISK" >> plain_$FILENAME
-    }
+      echo "$fROW,$bPATIENTID,$bSMOKER,$bSTROKE,$bLUNGCANCER,$bHEARTDISEASE,$bRISK" >> transit_files/plain_$FILENAME
+  }
 
     if [ $SCRIPTACTION = encrypt ] ; then
         encryptSourceCSV;
